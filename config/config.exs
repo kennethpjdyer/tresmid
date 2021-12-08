4a2f8f5b85c72c855a0d34f2ffc4eb0b8df6c066
Kenneth P. J. Dyer <kenneth.dyer@mongodb.com>
2021-12-08 17:43:52 -0500

    Cleanup

--- config/config.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-config :tresmid,
-  conf_path: Path.expand("~/.config/tresmid.yml"),
-  mongo_user: "tresmid",
-  mongo_pwd: "tresmid",
-  mongo_db: "tresmid",
-  mongo_host: "localhost",
-  mongo_port: 27017
--- test/tresmid_test.exs
+++
@@ -1,4 +0,0 @@
-defmodule TresmidTest do
-  use ExUnit.Case
-  doctest Tresmid
-end
